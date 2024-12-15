dd39ea6c33e448c1386754ebaa5f7845910f105b
chamelopi <ben.gorny@web.de>
2024-12-15 11:54:09 +0100

    add script for quickly setting up new container

---
+++ create-server.ps1
@@ -0,0 +1,11 @@
+$RESOURCE_GROUP="MinecraftServer"
+$STORAGE_ACCOUNT="minecraft20230716"
+$STORAGE_NAME="minecraft-server-phee"
+$CONTAINER_NAME="minecraft-server-phee"
+$IMAGE="serverimages.azurecr.io/minecraft-server"
+$STORAGE_PATH="/data"
+$HOSTNAME="cloud-mc-phee"
+
+
+az storage share-rm create -g $RESOURCE_GROUP --storage-account $STORAGE_ACCOUNT --name $STORAGE_NAME --quota 20 --enabled-protocols SMB --output table
+az container create -g $RESOURCE_GROUP --name $CONTAINER_NAME --image $IMAGE --azure-file-volume-share-name $STORAGE_NAME --azure-file-volume-account-name $STORAGE_ACCOUNT --azure-file-volume-mount-path "$STORAGE_PATH" --restart-policy Never --cpu 4 --memory 4 --dns-name-label $HOSTNAME --ports 25565 --os-type Linux -e "EULA=TRUE" "OPS=chamelopi" "TYPE=FABRIC" "VERSION=1.21.3" --location "west europe"
