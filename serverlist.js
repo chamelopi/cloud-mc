e8e295c873991953604d5d575ea7a79f6cce9017
chamelopi <ben.gorny@web.de>
2024-12-15 11:54:14 +0100

    add server for phee

--- serverlist.js
+++ serverlist.js
@@ -1,4 +1,5 @@
 export const SERVER_CHOICES = [
     { name: 'default', value: 'minecraft-server' },
     { name: 'moritz', value: 'minecraft-server-moritz' },
+    { name: 'phee', value: 'minecraft-server-phee' },
 ];
